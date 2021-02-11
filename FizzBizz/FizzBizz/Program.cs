290cab50a0f0bacc6dd38b3002399e9a3e5b1a3d
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-02-11 17:38:51 -0600

    Made the program to where it could find Fizz, Buzz, and Fizz Buzz

--- FizzBizz/FizzBizz/Program.cs
+++ FizzBizz/FizzBizz/Program.cs
@@ -6,6 +6,32 @@ namespace FizzBizz
     {
         static void Main(string[] args)
         {
+            Random r = new Random();
+            int randomNum = r.Next(1, 101); // Generate a random number between 1 and 100. Always do 1 more.
+            int divisableBy3Remainder = randomNum % 3;
+            bool isDivisibleBy3 = randomNum % 3 == 0;
+            
+            
+            if (randomNum % 3 == 0
+                && randomNum % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+
+            else if (randomNum % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            else if (randomNum % 3 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else
+            {
+                Console.WriteLine(randomNum);
+            }
+
             
         }
     }
